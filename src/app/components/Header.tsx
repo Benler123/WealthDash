381aad00ec188520e6979ff630cdf36db6dd2493
Tyler Kwok <tylerkwok@wealthfront.com>
2024-06-25 15:19:43 -0700

    fix header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,11 +4,8 @@ export default class Header extends Component {
   render() {
     return (
     <div className="flex flex-row w-full justify-center items-center">
-        <div className="relative mx-auto mt-5 h-24 w-80 justify-center">
-          <Image fill={true} src="/1.png" objectFit="cover" alt="logo"></Image>
-        </div>
-        <div className="relative mx-auto h-12 w-20">
-        <Image fill={true} src="/wflogo.png" objectFit="contain" alt="logo"></Image>
+        <div className="relative mx-auto mt-5 h-24 w-60 justify-center">
+          <Image fill={true} src="/1.png" objectFit="contain" alt="logo"></Image>
         </div>
       </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,7 @@ export default function Home() {
   const [expand, setExpand] = useState(false)
   return (
     <main className="min-h-screen bg-white">
-      <div className="flex flex-col h-30 w-full">
-        <div className="relative mx-auto h-20 w-80">
-          <Image fill={true} src="/1.png" objectFit="cover" alt="logo"></Image>
-        </div>
-        <div className="relative mx-auto w-80 text-black">
-          Week of:{" "}
-        </div>
-      </div>
+      <Header />
       <ResturantCards />
     </main>
   )
