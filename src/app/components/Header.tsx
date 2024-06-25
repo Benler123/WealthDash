604084aa50019fa4657246ce6b627af3e73258db
Tyler Kwok <tylerkwok@wealthfront.com>
2024-06-25 16:15:10 -0700

    fix head

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
     <div className="flex flex-row w-full justify-center items-center">
-        <div className="relative mx-auto mt-2 h-16c w-60 justify-center">
+        <div className="relative mx-auto mt-2 h-16 w-60 justify-center">
           <Image fill={true} src="/1.png" objectFit="contain" alt="logo"></Image>
         </div>
       </div>
