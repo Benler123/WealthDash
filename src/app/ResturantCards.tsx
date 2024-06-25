7e7b7caa629024b6229fff57bbffdef726815642
Tyler Kwok <tylerkwok@wealthfront.com>
2024-06-25 13:04:02 -0700

    update cards

--- src/app/ResturantCards.tsx
+++ src/app/ResturantCards.tsx
@@ -1,26 +1,73 @@
 import React, { Component } from 'react'
 import ResturantCard from './components/ResturantCard'
-export default class ResturantCards extends Component {
-  render() {
+function RestaurantCards(){    
+    const resturants = [
+        "McDonalds",
+        "Burger King",
+        "KFC",
+        "Wendys",
+        "Taco Bell",
+        "Pizza Hut",
+        "Subway",
+        "Starbucks",
+        "Dunkin Donuts",
+        "Chipotle",
+    ]
+    const images = [
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+        "download.jpeg",
+    ]
+    const ratings = [
+        3.2,
+        4.5,
+        2.7,
+        4.1,
+        3.9,
+        4.3,
+        3.8,
+        4.0,
+        4.2,
+        4.4
+    ]
+    const cuisines = [
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+        "Fast Food",
+    ]
+
     return (
 <div className="flex w-full justify-between flex-col">
     <div className="flex flex-row w-full justify-between pb-4">
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
+        {
+            resturants.slice(0,5).map((resturant, index) => (
+                <ResturantCard key={index} name={resturant} imagePath={images[index]} rating={ratings[index]} cuisine={cuisines[index]}/>
+            ))
+        }
     </div>
-    <div className="flex flex-row w-full justify-between">
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
-        <ResturantCard/>
+    <div className="flex flex-row w-full justify-between">{
+            resturants.slice(5,10).map((resturant, index) => (
+                <ResturantCard key={index} name={resturant} imagePath={images[index]} rating={ratings[index]} cuisine={cuisines[index]}/>
+            ))
+        }  
     </div>
 </div>
 
     )
   }
-}
+export default RestaurantCards;
 
--- src/app/components/ResturantCard.tsx
+++ src/app/components/ResturantCard.tsx
@@ -1,10 +1,17 @@
+'use client';
 import React, { Component } from 'react'
 import Grid from "@mui/material/Grid";
 import { Star } from 'lucide-react';
 import StarRating from './StarRating';
 
-export default class ResturantCard extends Component {
-  render() {
+interface ResturantCardProps {
+  name: string;
+  imagePath: string;
+  rating: number;
+  cuisine: string;
+}
+
+function ResturantCard(props: ResturantCardProps) {
     return (
       <div className="shadow-lg transition-transform duration-200 ease-in-out hover:scale-105" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '16rem', height: '21rem', borderRadius: '0.4rem', border: '2px solid rgb(70, 62, 188)', margin: '0.75rem' }}>
         <Grid container  justifyContent="center"  spacing={1} sx={{ height: "100%" }}>
@@ -15,7 +22,7 @@ export default class ResturantCard extends Component {
               flexDirection: "column",
             }}> 
             <div>
-              Title
+              {props.name}
             </div>
           </Grid>
           <Grid item xs={12} sx={{
@@ -24,7 +31,7 @@ export default class ResturantCard extends Component {
               alignItems: "center",
               flexDirection: "column",
             }}> 
-          <img src="download.jpeg" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }}/>         
+          <img src={props.imagePath} style={{ border: '2px solid rgb(70, 62, 188)' ,maxWidth: '100%', height: 'auto', objectFit: 'containx' }}/>         
            </Grid>
           <Grid item xs={12} sx={{
               display: "flex",
@@ -32,7 +39,7 @@ export default class ResturantCard extends Component {
               alignItems: "center",
               flexDirection: "column",
             }}> 
-            <StarRating rating={3.4}/>
+            <StarRating rating={props.rating}/>
           </Grid>
             <Grid item xs={12} sx={{
               display: "flex",
@@ -41,7 +48,7 @@ export default class ResturantCard extends Component {
               flexDirection: "column",
             }}> 
               <div>
-                cuisine
+                {props.cuisine}
               </div>
           </Grid>
         </Grid>
@@ -50,4 +57,4 @@ export default class ResturantCard extends Component {
       </div>
       )
   }
-}
+  export default ResturantCard;
