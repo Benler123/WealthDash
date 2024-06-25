83ca91ded82731dc9c968013f37866e19ef9a39a
Tyler Kwok <tylerkwok@wealthfront.com>
2024-06-25 12:39:48 -0700

    add card

---
+++ src/app/components/ResturantCard.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import Grid from "@mui/material/Grid";
+import { Star } from 'lucide-react';
+import StarRating from './StarRating';
+
+export default class ResturantCard extends Component {
+  render() {
+    return (
+      <div className="shadow-lg transition-transform duration-200 ease-in-out hover:scale-105" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '16rem', height: '21rem', borderRadius: '0.4rem', border: '2px solid rgb(70, 62, 188)', margin: '0.75rem' }}>
+        <Grid container  justifyContent="center"  spacing={1} sx={{ height: "100%" }}>
+          <Grid item xs={12} sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}> 
+            <div>
+              Title
+            </div>
+          </Grid>
+          <Grid item xs={12} sx={{
+              display: "flex",
+              justifyContent: "top",
+              alignItems: "center",
+              flexDirection: "column",
+            }}> 
+          <img src="download.jpeg" style={{ maxWidth: '100%', height: 'auto', objectFit: 'cover' }}/>         
+           </Grid>
+          <Grid item xs={12} sx={{
+              display: "flex",
+              justifyContent: "top",
+              alignItems: "center",
+              flexDirection: "column",
+            }}> 
+            <StarRating rating={3.2}/>
+          </Grid>
+            <Grid item xs={12} sx={{
+              display: "flex",
+              justifyContent: "top",
+              alignItems: "center",
+              flexDirection: "column",
+            }}> 
+              <div>
+                cuisine
+              </div>
+          </Grid>
+        </Grid>
+          
+        
+      </div>
+      )
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,16 @@
+"use client";
 import Image from "next/image";
+import ResturantCards from "./ResturantCards";
+import ResturantCard from "./components/ResturantCard";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main>
       <div className="flex flex-row h-24 w-full justify-between">
         <div className="text-white w-full mt-[5em]"><h1 className="text-center">MIRIAM DASH</h1></div>
       </div>
-      <div className="flex h-[40em] flex-col items-center justify-between p-24"><div className="flex flex-row h-24 w-full justify-between">
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-      </div>
-      <div className="flex flex-row h-24 w-full justify-between">
-      <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-        <div className="w-24 bg-slate-600"></div>
-      </div></div>
-      
+      <ResturantCards/>
+
     </main>
   );
 }
