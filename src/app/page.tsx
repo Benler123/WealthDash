68cbb4c2975171eec09d1fb8c579bcc4fcec31c0
Austin Nicola Ardisaputra <austinardisaputra@wealthfront.com>
2024-06-25 13:13:10 -0700

    substitute with real cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import CachedIcon from '@mui/icons-material/Cached';
 import React, { useState } from 'react';
 import exp from "constants";
 import ResturantCards from "./ResturantCards";
-import ResturantCard from "./components/ResturantCard";
+import ResturantCard from './components/ResturantCard'
+// import RestaurantCard from "@/components/RestaurantCard";
 
 export default function Home() {
   const [expand, setExpand] = useState(false);
@@ -15,20 +16,20 @@ export default function Home() {
         <div className="relative mx-auto h-[4em] w-80 text-black">Week of: </div>
       </div>
       <div className="flex flex-col justify-between h-[24em]">
-        <div className="flex flex-row h-24 w-full justify-between">
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[88.5em] translate-y-[18em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[70.8em] translate-y-[18em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[53.1em] translate-y-[18em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[35.4em] translate-y-[18em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[17.7em] translate-y-[18em]"}`}></div>
+        <div className="flex flex-row w-full justify-between">
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[88.5em] translate-y-[18em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[70.8em] translate-y-[18em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[53.1em] translate-y-[18em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[35.4em] translate-y-[18em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[17.7em] translate-y-[18em]"}`}><ResturantCard /></div>
           <div className="w-24 bg-indigo-500 text-5xl" onClick={() => setExpand(!expand)}><CachedIcon fontSize="inherit"/></div>
         </div>
-        <div className="flex flex-row h-24 w-full justify-between">
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[88.5em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[70.8em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[53.1em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[35.4em]"}`}></div>
-          <div className={`transition ease-in-out w-24 bg-slate-600 ${expand && "translate-x-[17.7em]"}`}></div>
+        <div className="flex flex-row h-auto w-full justify-between">
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[88.5em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[70.8em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[53.1em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[35.4em]"}`}><ResturantCard /></div>
+          <div className={`transition ease-in-out w-auto ${expand && "translate-x-[17.7em]"}`}><ResturantCard /></div>
           <div className="w-24 bg-indigo-500"></div>
         </div>
       </div>
