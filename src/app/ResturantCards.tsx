8cd294966556e9022839f0d79625bca7273b4d0b
Tyler Kwok <tylerkwok@wealthfront.com>
2024-06-25 14:39:21 -0700

    only select 5

--- src/app/ResturantCards.tsx
+++ src/app/ResturantCards.tsx
@@ -1,6 +1,7 @@
 import React, { Component, useState } from 'react';
 import ResturantCard from './components/ResturantCard';
 import CachedIcon from '@mui/icons-material/Cached';
+import { IntegerType } from 'mongodb';
 
 function RestaurantCards(){    
     const resturants = [
@@ -50,17 +51,33 @@ function RestaurantCards(){
         "Fast Food",
         "Fast Food",
         "Fast Food",
-    ]
+    ]   
+
 
     const [expand, setExpand] = useState(false);
+  const [selected, setSelected] = useState(new Array(resturants.length).fill(false));
+
+  const toggleSelection = (index: number) => {
+    if (selected.filter((s) => s).length === 5 && !selected[index]) {
+      return;
+    }
+    const newSelected = [...selected];
+    newSelected[index] = !newSelected[index];
+    setSelected(newSelected);
+    console.log(index)
+  };
+
+  const selectedColor = "rgb(196, 83, 0)";
+  const wfColor = "rgb(70,62,188)"
+  const numSelected = selected.filter((s) =>s).length;
 
     return (
 <div className="flex w-full justify-between flex-col">
     <div className="flex flex-row w-full justify-between pb-4">
         {
             resturants.slice(0,5).map((resturant, index) => (
-                 <div className={`transition ease-in-out w-auto ${expand && `translate-x-["${17.7 * (5 - index)}"em] translate-y-[18em]`}`}>
-                <ResturantCard key={index} name={resturant} imagePath={images[index]} rating={ratings[index]} cuisine={cuisines[index]}/>
+                 <div onClick={()=>toggleSelection(index)} className={`transition ease-in-out w-auto ${expand && `translate-x-["${17.7 * (5 - index)}"em] translate-y-[18em]`}`}>
+                <ResturantCard key={index} name={resturant} imagePath={images[index]} rating={ratings[index]} cuisine={cuisines[index]} borderColor={selected[index]? selectedColor: wfColor}/>
                 </div>
             ))
         }
@@ -69,8 +86,8 @@ function RestaurantCards(){
     </div>
     <div className="flex flex-row w-full justify-between">{
             resturants.slice(5,10).map((resturant, index) => (
-                <div className={`transition ease-in-out w-auto ${expand && `translate-x-["${17.7 * (10 - index)}"em]`}`}>
-                <ResturantCard key={index} name={resturant} imagePath={images[index]} rating={ratings[index]} cuisine={cuisines[index]}/>
+                <div  onClick={()=>toggleSelection(index + 5)} className={`transition ease-in-out w-auto ${expand && `translate-x-["${17.7 * (10 - index)}"em]`}`}>
+                <ResturantCard key={index} name={resturant} imagePath={images[index + 5]} rating={ratings[index + 5]} cuisine={cuisines[index + 5]} borderColor={selected[index + 5]? selectedColor: wfColor}/>
                 </div>
             ))
         }  
